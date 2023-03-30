30b873c290507c7f1e660ea35ee2ca7d2ab3621c
SAMEER SHARMA <sameervasu2001@gmail.com>
2023-03-30 15:39:09 +0530

    update

---
+++ form/src/component/Form.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { TextField } from '@mui/material'
+import Grid from '@mui/material/Grid';
+
+function Form() {
+    return (
+        <>
+            <Grid container >
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                />
+
+            </Grid>
+
+
+
+
+
+        </>
+    )
+}
+
+export default Form
